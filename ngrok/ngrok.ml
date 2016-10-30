101fdd9a23c929a1ddbdc71977112a3f6ea5cc96
Jun Furuse <Jun.Furuse@gmail.com>
2016-10-30 16:08:57 +0000

    rewrite ngrok.ml ngrok.ml: submit is now disabled while processing

--- ngrok/ngrok.ml
+++ ngrok/ngrok.ml
@@ -3,34 +3,45 @@ open Xjs
 let server = "https://5822cdcc.ngrok.io"
 (* let server = "http://localhost:8080" *)
 
-let query () =
-  (* XXX we should catch errors *)
-  let q        = Js.to_string & (Coerce.input & getById "q")        ##.value in
-  let packtype = Js.to_string & (Coerce.select & getById "packtype") ##.value in
-  let packs    = Js.to_string & (Coerce.input & getById "packs")    ##.value in
-  let url = (Printf.sprintf "%s/?%s"
-                        server
-                        (Url.encode_arguments [ "q", q
-                                              ; "packtype", packtype
-                                              ; "packs", packs])) in
-  let insert = getById "insert" in
-  remove_children insert;
-  let wait = getById "wait" in
-  wait##setAttribute (js "style") (js "display: block;");
-  XmlHttpRequest.get url
-  >>= fun hf ->
-  wait##setAttribute (js "style") (js "display: none;");
-  insert##.innerHTML := js hf.content;
-  Lwt.return ()
-    
 let form () =
-  let form = Coerce.form & getById "form" in
+  let q        = Coerce.input & getById "q" in
+  let packtype = Coerce.select & getById "packtype" in
+  let packs    = Coerce.input & getById "packs" in
+  let form     = Coerce.form & getById "form" in
+  let submit   = Coerce.input & getById "submit" in
+  let insert   = getById "insert" in
+  let wait     = getById "wait" in
+
+  let query () =
+    let url =
+      let q        = Js.to_string q##.value in
+      let packtype = Js.to_string packtype##.value in
+      let packs    = Js.to_string packs##.value in
+      Printf.sprintf "%s/?%s"
+        server
+        (Url.encode_arguments [ "q"        , q
+                              ; "packtype" , packtype
+                              ; "packs"    , packs])
+    in
+    submit##.disabled := Js._true;
+    remove_children insert;
+    wait##setAttribute (js "style") (js "display: block;");
+    XmlHttpRequest.get url
+
+    >>= fun hf ->
+
+    submit##.disabled := Js._false;
+    wait##setAttribute (js "style") (js "display: none;");
+    insert##.innerHTML := js hf.content;
+    Lwt.return ()
+  in
+    
   form##.onsubmit := (Dom.handler (fun _ ->
     catch_and_alert_ (fun () -> ignore & query ());
     Js._false))
     
 let start _ =
-  form ();
+  catch_and_alert_ form;
   Js._false
 
 let () = Dom_html.window##.onload := Dom_html.handler start
